3d941d65428ed5fda2c71af8e5d811bac044c8c9
ayush-562 <190080334+ayush-562@users.noreply.github.com>
2025-04-14 12:50:03 +0530

    Complete About Page

--- src/landing_page/about/Team.jsx
+++ src/landing_page/about/Team.jsx
@@ -2,22 +2,22 @@ export default function Stats() {
     return (
         <>
             <div className="container">
-                <div className="row p-5 m-5 border-top">
+                <div className="row">
                     <h2 className="text-center fw-normal">
                         People
                     </h2>
                 </div>
-                <div className="row p-5  m-5">
-                    <div className="col-5 p-5">
-                        <img class="founder-image" src="/static/images/nithin-kamath.jpg" alt="Nithin Kamath, CEO" />
-                        <h5>Nithin Kamath</h5>
-                        <p className="text-muted">Founder, CEO</p>
+                <div className="row p-5 mx-5 text-muted" style={{lineHeight:"1.8"}}>
+                    <div className="col-5 p-5 pe-0 text-center">
+                        <img src="/media/images/nithinKamath.jpg" alt="Nithin Kamath, CEO" style={{ borderRadius: "100%", width: "70%" }} />
+                        <h5 className="mt-3">Nithin Kamath</h5>
+                        <p className="mt-4">Founder, CEO</p>
                     </div>
-                    <div className="col-7 p-5 mt-5 text-muted lh-lg">
+                    <div className="col-7 p-5 ps-0 text-muted lh-lg">
                         <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry. </p>
                         <p>He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
                         <p>Playing basketball is his zen.</p>
-                        <p>Connect on Homepage / TradingQnA / Twitter</p>
+                        <p>Connect on <a href="" style={{textDecoration:"none"}}>Homepage</a> / <a href="" style={{textDecoration:"none"}}>TradingQnA</a> / <a href="" style={{textDecoration:"none"}}>Twitter</a></p>
                     </div>
                 </div>
             </div>
